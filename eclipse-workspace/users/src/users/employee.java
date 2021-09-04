1fce7a1bbbc309d8b41714ea787fc487b24c4471
Sudheer Palanchawar <sudheer.palanchawar@diplsrv.local>
2021-09-04 12:26:36 +0530

    Sudheer

---
+++ eclipse-workspace/users/src/users/employee.java
@@ -0,0 +1,13 @@
+package users;
+
+public class employee {
+	public static void main(String[] args) {
+	int a;
+	a=10;
+     int b;
+	b=20;
+	int Sum;
+	Sum = (a+b);
+		System.out.println(Sum);
+	}
+}
